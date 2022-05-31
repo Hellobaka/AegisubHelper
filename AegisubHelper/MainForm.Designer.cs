5d78e1b055654d00c2cc407067386ab886087098
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-05-31 09:54:03 +0800

    杂项

--- AegisubHelper/MainForm.Designer.cs
+++ AegisubHelper/MainForm.Designer.cs
@@ -33,8 +33,11 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.processLoadStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.memoryStatus = new System.Windows.Forms.ToolStripStatusLabel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.RecordBtn = new System.Windows.Forms.Button();
             this.translatedText = new System.Windows.Forms.RichTextBox();
+            this.HotKeyStatus = new System.Windows.Forms.Label();
+            this.networkProgress = new System.Windows.Forms.ProgressBar();
+            this.networkProcessLabel = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,9 +62,9 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.processLoadStatus,
             this.memoryStatus});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 341);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(411, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -77,15 +80,15 @@
             this.memoryStatus.Size = new System.Drawing.Size(77, 17);
             this.memoryStatus.Text = "等待初始化...";
             // 
-            // button1
+            // RecordBtn
             // 
-            this.button1.Location = new System.Drawing.Point(306, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.RecordBtn.Location = new System.Drawing.Point(306, 12);
+            this.RecordBtn.Name = "RecordBtn";
+            this.RecordBtn.Size = new System.Drawing.Size(75, 23);
+            this.RecordBtn.TabIndex = 3;
+            this.RecordBtn.Text = "录制";
+            this.RecordBtn.UseVisualStyleBackColor = true;
+            this.RecordBtn.Click += new System.EventHandler(this.Record_Click);
             // 
             // translatedText
             // 
@@ -95,18 +98,52 @@
             this.translatedText.TabIndex = 4;
             this.translatedText.Text = "";
             // 
+            // HotKeyStatus
+            // 
+            this.HotKeyStatus.AutoSize = true;
+            this.HotKeyStatus.Location = new System.Drawing.Point(306, 44);
+            this.HotKeyStatus.Name = "HotKeyStatus";
+            this.HotKeyStatus.Size = new System.Drawing.Size(68, 17);
+            this.HotKeyStatus.TabIndex = 5;
+            this.HotKeyStatus.Text = "不捕获模式";
+            // 
+            // networkProgress
+            // 
+            this.networkProgress.Location = new System.Drawing.Point(306, 93);
+            this.networkProgress.MarqueeAnimationSpeed = 30;
+            this.networkProgress.Name = "networkProgress";
+            this.networkProgress.Size = new System.Drawing.Size(94, 23);
+            this.networkProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.networkProgress.TabIndex = 6;
+            this.networkProgress.Visible = false;
+            // 
+            // networkProcessLabel
+            // 
+            this.networkProcessLabel.AutoSize = true;
+            this.networkProcessLabel.Location = new System.Drawing.Point(306, 73);
+            this.networkProcessLabel.Name = "networkProcessLabel";
+            this.networkProcessLabel.Size = new System.Drawing.Size(65, 17);
+            this.networkProcessLabel.TabIndex = 7;
+            this.networkProcessLabel.Text = "获取结果...";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(411, 363);
+            this.Controls.Add(this.networkProcessLabel);
+            this.Controls.Add(this.networkProgress);
+            this.Controls.Add(this.HotKeyStatus);
             this.Controls.Add(this.translatedText);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RecordBtn);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.playText);
             this.Controls.Add(this.processTitle);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "AegisubHelper";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
@@ -122,7 +159,10 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel processLoadStatus;
         private System.Windows.Forms.ToolStripStatusLabel memoryStatus;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button RecordBtn;
         private System.Windows.Forms.RichTextBox translatedText;
+        private System.Windows.Forms.Label HotKeyStatus;
+        private System.Windows.Forms.ProgressBar networkProgress;
+        private System.Windows.Forms.Label networkProcessLabel;
     }
 }
--- AegisubHelper/MainForm.cs
+++ AegisubHelper/MainForm.cs
@@ -1,15 +1,9 @@
-﻿using Memory;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
+using PInvoke;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AegisubHelper
@@ -20,11 +14,56 @@ namespace AegisubHelper
         {
             InitializeComponent();
         }
+        [Flags]
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            // Either WINDOWS key was held down. These keys are labeled with the Windows logo.
+            // Keyboard shortcuts that involve the WINDOWS key are reserved for use by the
+            // operating system.
+            Windows = 8
+        }
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == (int)User32.WindowMessage.WM_HOTKEY)
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case 0x64:
+                        AutoRecord = !AutoRecord;
+                        if (AutoRecord)
+                        {
+                            HotKeyStatus.Text = "自动捕获模式";
+                        }
+                        else
+                        {
+                            HotKeyStatus.Text = "不捕获模式";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            base.WndProc(ref m);
+        }
         public bool SearchProcessFlag { get; set; } = true;
         public static Process AegisubProcess { get; set; }
-        private async void MainForm_Load(object sender, EventArgs e)
+        public bool AutoRecord { get; set; }
+        public bool PlayStatus { get; set; }
+        public bool RecordStatus { get; set; }
+        private void MainForm_Load(object sender, EventArgs e)
         {
-            new Thread(() =>
+            networkProcessLabel.Visible = false;
+            RegisterHotKey(this.Handle, 100, KeyModifiers.Control, Keys.F9);
+
+            new Thread(async () =>
             {
                 while (SearchProcessFlag)
                 {
@@ -56,7 +95,20 @@ namespace AegisubHelper
                             else
                             {
                                 bool r = MemoryHelper.IsPlaying();
-                                playText.Text = r ? "正在播放" : "未播放";
+                                if (AutoRecord && PlayStatus != r)
+                                {
+                                    CallRecordAndTranslate(RecordStatus);
+                                    RecordStatus = !RecordStatus;
+                                }
+                                PlayStatus = r;
+                                if (PlayStatus)
+                                {
+                                    playText.Text = "正在播放";
+                                }
+                                else
+                                {
+                                    playText.Text = "未播放";
+                                }
                             }
                         }
                         else
@@ -79,30 +131,46 @@ namespace AegisubHelper
                 }
             }).Start();
         }
-        bool flag = false;
-        private async void button1_Click(object sender, EventArgs e)
+        public async void CallRecordAndTranslate(bool r)
         {
-            if (flag)
+            if (r)
             {
-                flag = false;
                 AudioHelper.StopRecord();
-                var text = await YouDaoAPI.Translate(AudioHelper.outputFilePath);
-                JObject json = JObject.Parse(await text.Content.ReadAsStringAsync());
-                if (((int)json["errorCode"]) == 0)
+                RecordBtn.Text = "录制";
+                networkProgress.Visible = true;
+                networkProcessLabel.Visible = true;
+                var origin = await YouDaoAPI.Voice2Text(AudioHelper.outputFilePath);
+                string translate = "";
+                if (origin != "err")
                 {
-                    translatedText.Text += json["query"] + "\n";
-                    translatedText.Text += json["translation"] + "\n";
+                    translate = await BaiduAPI.TextTranslate(origin);                    
                 }
-                else
+                BeginInvoke(new MethodInvoker(() =>
                 {
-                    translatedText.Text += json.ToString(Newtonsoft.Json.Formatting.Indented);
-                }
+                    networkProgress.Visible = false;
+                    networkProcessLabel.Visible = false;
+                    translatedText.Text += origin + "\n";
+                    translatedText.Text += translate + "\n\n";
+                    translatedText.Select(translatedText.Text.Length, 0);
+                    translatedText.ScrollToCaret();
+                }));
             }
             else
             {
-                flag = true;
                 AudioHelper.StartRecord();
+                RecordBtn.Text = "停止录制";
             }
+        }
+        private async void Record_Click(object sender, EventArgs e)
+        {
+            CallRecordAndTranslate(RecordStatus);
+            RecordStatus = !RecordStatus;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SearchProcessFlag = false;
+            UnregisterHotKey(Handle, 100);
         }
     }
 }
