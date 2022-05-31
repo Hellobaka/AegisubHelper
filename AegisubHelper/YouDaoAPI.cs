e8db7a6b6fb3cfc0f32f70d25b8e043fd2c26656
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-05-31 09:53:36 +0800

    添加有道语音识别接口

--- AegisubHelper/YouDaoAPI.cs
+++ AegisubHelper/YouDaoAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace AegisubHelper
 {
     internal class YouDaoAPI
     {
-        public static async Task<HttpResponseMessage> Translate(string path)
+        public static async Task<string> VoiceTranslate(string path)
         {
             string url = "https://openapi.youdao.com/speechtransapi";
             Dictionary<string, string> dic = new Dictionary<string, string>();
@@ -47,7 +48,63 @@ namespace AegisubHelper
             using HttpClient client = new();
             var formStr = string.Join('&', dic.Select(kv => $"{kv.Key}={HttpUtility.UrlEncode(kv.Value)}"));
             var content = new StringContent(formStr, Encoding.UTF8, "application/x-www-form-urlencoded");
-            return client.PostAsync(url, content).Result;            
+            var response = await client.PostAsync(url, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+        public static async Task<string> Voice2Text(string path)
+        {
+            string url = "https://openapi.youdao.com/asrapi";
+            Dictionary<string, string> dic = new ();
+            
+            if (File.Exists(path) == false)
+                throw new FileNotFoundException("No audio file found");
+            string base64 = Convert.ToBase64String(File.ReadAllBytes(path));
+
+            string appKey = Config.GetConfig<string>("Youdao_AppId");
+            string appSecret = Config.GetConfig<string>("Youdao_Key");
+            string format = "wav";
+            string rate = "16000";
+            string channel = "1";
+            string type = "1";
+            string salt = Guid.NewGuid().ToString();
+            string curtime = Helper.TimeStamp.ToString();
+            string signStr = appKey + Truncate(base64) + salt+ curtime + appSecret;
+            string sign = Helper.SHA256(signStr);
+            
+            dic.Add("q", base64);
+            dic.Add("langType", "ja");
+            dic.Add("appKey", appKey);
+            dic.Add("format", format);
+            dic.Add("rate", rate);
+            dic.Add("channel", channel);
+            dic.Add("type", type);
+            dic.Add("salt", salt);
+            dic.Add("sign", sign);
+            dic.Add("signType", "v2");
+            dic.Add("curtime", curtime);
+            
+            using HttpClient client = new();
+            var formStr = string.Join('&', dic.Select(kv => $"{kv.Key}={HttpUtility.UrlEncode(kv.Value)}"));
+            var content = new StringContent(formStr, Encoding.UTF8, "application/x-www-form-urlencoded");
+            var result = await client.PostAsync(url, content);
+            var json = JObject.Parse(await result.Content.ReadAsStringAsync());
+            if (((int)json["errorCode"]) == 0)
+            {
+                return json["result"][0].ToString();
+            }
+            else
+            {
+                return "err";
+            }
+        }
+        private static string Truncate(string q)
+        {
+            if (q == null)
+            {
+                return string.Empty;
+            }
+            int len = q.Length;
+            return len <= 20 ? q : (q.Substring(0, 10) + len + q.Substring(len - 10, 10));
         }
     }
 }
